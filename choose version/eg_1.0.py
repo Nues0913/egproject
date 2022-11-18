1f36c06067acd14d9016cd093a20bd81becda50b
Nues0913 <a09871530274@gmail.com>
2022-11-18 09:50:28 +0800

    20%

--- choose version/eg_1.0.py
+++ choose version/eg_1.0.py
@@ -2,8 +2,10 @@ import os
 import time
 import random
 import openpyxl as op
+'''
 wb = op.Workbook()
 wb.save('test.xlsx')
-eg_data = op.load_workbook('en')
+'''
+eg_data = op.load_workbook('en.xlsx')
 #sheet name: 最近遇到 疑難雜症 lv3~4特選
 print(eg_data['最近遇到'])
